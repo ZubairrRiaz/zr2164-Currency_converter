60d37d5cbe2175dc1b92d67d00d6c4c475fbc0d2
ZubairrRiaz <160521830+ZubairrRiaz@users.noreply.github.com>
2024-04-11 12:18:08 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+import inquirer from 'inquirer';
+const currencyRates = {
+    USD: 1, //BASE CURRENCY
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280,
+};
+const question_1 = await inquirer.prompt([
+    {
+        name: 'from',
+        message: 'Enter From Currency!',
+        type: 'list',
+        choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR'],
+    },
+    {
+        name: 'To',
+        message: 'Enter To Currency!',
+        type: 'list',
+        choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR'],
+    },
+    {
+        name: 'amount',
+        type: 'number',
+        message: 'Enter your amount.',
+    }
+]);
+let fromAmount = currencyRates[question_1.from]; // USD TO  
+let toAmount = currencyRates[question_1.To]; // PKR        
+let amount = question_1.amount; //AMOUNT 1000   
+let baseAmount = amount / fromAmount; //1000 / 1 = 1000   
+let convertedAmount = Math.floor(baseAmount * toAmount); // 1000 * 280 = 280000,  74.57=280=25000000
+console.log(convertedAmount); // FINAL RESULT = 280000;
